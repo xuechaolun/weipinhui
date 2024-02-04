62b59f53d54951e25ce42b1dcb7b3b39a83c6cc3
xcl <718484458@qq.com>
2024-02-04 22:38:35 +0800

    update

--- shop_list_async_v2.py
+++ shop_list_async_v2.py
@@ -12,15 +12,16 @@ import shop_list
 async def task(browser: Browser, url):
     page = await browser.new_page()
     await page.goto(url)
+    await page.evaluate('window.scrollTo(0, 3000)')
+    await asyncio.sleep(3)
     print(url)
     await page.screenshot(path=f'{url}.png')
-    await browser.close()
 
 
 # 创建一个浏览器，并打开多个标签页面
 async def main():
     async with async_playwright() as apw:
-        browser = await apw.firefox.launch(headless=True)
+        browser = await apw.chromium.launch(headless=False)
         # 单个task
         # await task(browser, 'https://list.vip.com/autolist.html?rule_id=59450718')
 
@@ -28,6 +29,7 @@ async def main():
         # 通过协程把多个url分发到多个页面上打开访问
         await asyncio.wait([asyncio.create_task(task(browser, shop_type['shop_type_url'])) for shop_type in
                             shop_list.get_shop_type_list()[500:520]])
+        await browser.close()
 
 
 if __name__ == '__main__':
